975d84cce1f0be62a72b45c2d98c03d15772aa71
AlexOvey <miekie1408@gmail.com>
2019-08-06 00:19:37 +0200

    second-commit

--- start.py
+++ start.py
@@ -1,4 +1,8 @@
 print("welcome to python for home automation")
 word = ["word-list", "1", "2"]
+word_2 = ["list", "31", "25"]
 for i in word:
-    print(i)
+    for j in word_2:
+        a = [i+j]
+        print(a)
+    
